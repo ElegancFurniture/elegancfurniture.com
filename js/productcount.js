b8ad437c8be0286c70bfc22b3ca254c88fbd4828
Irriva <115064063+Irriva@users.noreply.github.com>
2024-10-29 02:54:37 +0530

    Fixed the product count not showing up on category pages
    They were not showing up when category pages were accessed directly without visiting the index page , because the variables were set there , To fix it I added a script that checks if variables are set or not and sets them if not to all category pages

---
+++ js/productcount.js
@@ -0,0 +1,11 @@
+if (!localStorage.getItem('PremiumExecChairCount')) {
+    localStorage.setItem('PremiumExecChairCount', 56);
+    localStorage.setItem('ExecChairCount', 22);
+    localStorage.setItem('WorkstationCount', 11);
+    localStorage.setItem('Others', 1);
+    localStorage.setItem('Parts', 9);
+    localStorage.setItem('BarStools', 13);
+    localStorage.setItem('OfficeSofas', 6);
+    localStorage.setItem('WaitingChairs', 5);
+    localStorage.setItem('CafeteriaChairs', 10);
+}
